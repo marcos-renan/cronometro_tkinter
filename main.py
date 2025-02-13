27bffd0f6451945fa945b286d89efef18128a69e
Marcos <devmarcos7@gmail.com>
2025-02-13 15:16:13 -0300

    criando os botoes

--- main.py
+++ main.py
@@ -18,7 +18,16 @@ label_nome = tk.Label(win, text='Cronômetro', fon=('Arial 10'), bg=preto, fg=br
 label_nome.place(x=20, y=5) #localidade dentro da janela
 
 label_tempo = tk.Label(win, text='00:00:00', fon=('Times 50 bold'), bg=preto, fg=azul) #cronometro
-label_tempo.place(x=20, y=25) #localidade dentro da janela
+label_tempo.place(x=20, y=30) #localidade dentro da janela
+
+botao_iniciar = tk.Button(win, text='Iniciar', width=10, height=2, bg=preto, fg=branco, font=('Ivy 8 bold'), relief='raised', overrelief='ridge') #botao para iniciar o cronometro
+botao_iniciar.place(x=20, y=130) #localidade do botao na janela
+
+botao_parar = tk.Button(win, text='Parar', width=10, height=2, bg=preto, fg=branco, font=('Ivy 8 bold'), relief='raised', overrelief='ridge') #botao para parar o cronometro
+botao_parar.place(x=105, y=130) #localidade do botao na janela
+
+botao_reiniciar = tk.Button(win, text='Reiniciar', width=10, height=2, bg=preto, fg=branco, font=('Ivy 8 bold'), relief='raised', overrelief='ridge') #botao para reiniciar o cronometro
+botao_reiniciar.place(x=190, y=130) #localidade do botao na janela
 
 
 
